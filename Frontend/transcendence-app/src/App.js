32bfb56262b07edab3d15abbdaf86b5b455902fb
Zelinsta <theotimeq@gmail.com>
2023-05-10 16:59:56 +0200

    makefile et docker front

--- Frontend/transcendence-app/src/App.js
+++ Frontend/transcendence-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Coucou to reload.
+          Edit <code>src/App.js</code> COucou
         </p>
         <a
           className="App-link"
