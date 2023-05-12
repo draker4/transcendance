4fc2499bd0d613d1f1d8e0838b49b711bed9ad61
bperriol <bperriol@bperriol42.myguest.virtualbox.org>
2023-05-12 16:36:13 +0200

    ok

--- Dockers/Database/tools/start_db.sh
+++ Dockers/Database/tools/start_db.sh
@@ -23,17 +23,11 @@ else
 
 	echo "loading migrate.sql..."
 	psql -U postgres -d postgres -a -f migrate.sql
-
 	echo "migrate.sql loaded!"
 
 	echo "add addresses in configuration files"
 	sed -i "s/port = .*/port = $DATA_BASE_PORT/" /etc/postgresql/13/main/postgresql.conf
 	sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" /etc/postgresql/13/main/postgresql.conf
-
-	echo "loading migrate.sql..."
-	psql -U postgres -d postgres -a -f migrate.sql
-
-	echo "migrate.sql loaded!"
 
 	pg_ctlcluster 13 main stop
 
