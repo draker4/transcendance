f0eb9d1c1625a2e0a6ff8a60b46d8f5482a8eded
Zelinsta <theotimeq@gmail.com>
2023-05-12 18:07:39 +0200

    Database

--- Dockers/Database/tools/start_db.sh
+++ Dockers/Database/tools/start_db.sh
@@ -7,25 +7,28 @@ echo "Begin Database configuration"
 # sed -i "s/DATA_BASE_PASSWORD/$DATA_BASE_PASSWORD/g" ./migrate.sql
 # sed -i "s/DATA_BASE_USER/$DATA_BASE_USER/g" ./migrate.sql
 
-# mkdir -p /etc/postgresql/13/data
-# chmod -R 750 /etc/postgresql/13/data 
+mkdir -p /etc/postgresql/13/
+mkdir -p /var/lib/postgresql/13/
+chmod 777 /var/lib/postgresql/13/
 
-echo "adding postgres command to path"
+echo "---------------Adding postgres command to path----------"
 export PATH+=:/usr/lib/postgresql/13/bin
 
-Si premier demarage , on configure la database
+# Si premier demarage , on configure la database
 if [ ! -d "/etc/postgresql/13/conf_done" ]; then
-    echo "---------No database found, creating one----------------"
+    echo "---------No database found, creating one------------"
 
     pg_ctlcluster 13 main start
 
-    echo "loading migrate.sql..."
+    echo "---------------Loading migrate.sql...---------------"
     psql -U postgres -d postgres -a -f migrate.sql
-    echo "migrate.sql loaded!"
+    echo "---------------Migrate.sql loaded!------------------"
 
     pg_ctlcluster 13 main stop
 
+    #On change la ou s'enregistre les fichier de la bs , idealement dans le volume....
     # sed -i "s|data_directory = '/var/lib/postgresql/13/main'|data_directory = '/etc/postgresql/13/data'|" /etc/postgresql/13/main/postgresql.conf   
+    
     sed -i "s/port = .*/port = $DATA_BASE_PORT/" /etc/postgresql/13/main/postgresql.conf
     sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" /etc/postgresql/13/main/postgresql.conf
     echo "host  all  all 0.0.0.0/0 md5" >> /etc/postgresql/13/main/pg_hba.conf
@@ -37,6 +40,6 @@ if [ ! -d "/etc/postgresql/13/conf_done" ]; then
     service postgresql stop
 fi
 
-echo "starting postgresql service"
+echo "---------------Starting postgresql service---------------"
 postgres -D /etc/postgresql/13/main
 
