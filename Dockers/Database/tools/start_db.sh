0baa7e22403d060c31752514abe33c56fdd80250
Zelinsta <theotimeq@gmail.com>
2023-05-12 15:36:47 +0200

    Postgres docker + start_db.sh

--- Dockers/Database/tools/start_db.sh
+++ Dockers/Database/tools/start_db.sh
@@ -3,38 +3,40 @@
 echo "Begin Database configuration"
 
 # Change les variable dans le script d'initialisation de la db
-sed -i "s/DATA_BASE_NAME/$DATA_BASE_NAME/g" ./migrate.sql
-sed -i "s/DATA_BASE_PASSWORD/$DATA_BASE_PASSWORD/g" ./migrate.sql
-sed -i "s/DATA_BASE_USER/$DATA_BASE_USER/g" ./migrate.sql
-sed -i "s/port = .*/port = $DATA_BASE_PORT/" /etc/postgresql/13/main/postgresql.conf
+# sed -i "s/DATA_BASE_NAME/$DATA_BASE_NAME/g" ./migrate.sql
+# sed -i "s/DATA_BASE_PASSWORD/$DATA_BASE_PASSWORD/g" ./migrate.sql
+# sed -i "s/DATA_BASE_USER/$DATA_BASE_USER/g" ./migrate.sql
+
 
 echo "adding postgres command to path"
-echo "export PATH+=:/usr/lib/postgresql/13/bin" >> ~/.bashrc
+# echo "export PATH+=:/usr/lib/postgresql/13/bin" >> ~/.bashrc
 export PATH+=:/usr/lib/postgresql/13/bin
 
-if [ ! -d "/var/lib/postgresql/data" ]; then
+# if [ ! -d "/var/lib/postgresql/13/main" ]; then
 
-	echo "no user or database created"
-	pg_ctlcluster 13 main start
+echo "No database found, creating one"
 
-	echo "creating user"
-	#createuser crunchy
+pg_ctlcluster 13 main start
 
-	echo "creating database"
-	#createdb crunchydb -O crunchy
+echo "loading migrate.sql..."
+psql -U postgres -d postgres -a -f migrate.sql
 
-	echo "give acces to the user"
-	#psql -c "alter user crunchy with encrypted password 'crunchypass';"
-	#psql -c "grant all privileges on database crunchydb to crunchy;"
-	#psql -c "revoke all privileges on database crunchydb from postgres;"
+echo "migrate.sql loaded!"
 
-	#Load le script d'initialisation de la db
-    # echo "loading migrate.sql"
-	psql -U crunchy -d crunchydb -a -f migrate.sql
+pg_ctlcluster 13 main stop
 
-	pg_ctlcluster 13 main stop
-fi
+sed -i "s/port = .*/port = $DATA_BASE_PORT/" /etc/postgresql/13/main/postgresql.conf
+sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" /etc/postgresql/13/main/postgresql.conf
+
+echo "host  all  all 0.0.0.0/0 md5" >> /etc/postgresql/13/main/pg_hba.conf
+
+service postgresql restart
+service postgresql stop
+
+# fi
+
+chmod 777 /var/lib/postgresql/13
 
 echo "starting postgresql service"
-#tail -f /dev/null
 postgres -D /etc/postgresql/13/main
+
