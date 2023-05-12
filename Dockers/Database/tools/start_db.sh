36351e14b728a907cde986100fbd70869c392b25
bperriol <bperriol@bperriol42.myguest.virtualbox.org>
2023-05-12 11:48:37 +0200

    before merge

--- Dockers/Database/tools/start_db.sh
+++ Dockers/Database/tools/start_db.sh
@@ -11,14 +11,23 @@ if [ ! -d "/var/lib/postgresql/data" ]; then
 	echo "no user or database created"
 	pg_ctlcluster 13 main start
 
-	echo "creating user"
-	createuser crunchy
-
 	echo "creating database"
+	#postfres -c "create database crunchydb;"
+	createuser crunchy
 	createdb crunchydb -O crunchy
+
+	echo "creating user"
+	#postgres -c "create user crunchy with encrypted password 'crunchypass';"
+
+	#echo "add password to the superuser"
+	#echo "give acces to the user"
+	psql -c "alter user crunchy with encrypted password 'crunchypass';"
+	psql -c "grant all privileges on database crunchydb to crunchy;"
+	psql -c "revoke all privileges on database crunchydb from postgres;"
 
 	pg_ctlcluster 13 main stop
 fi
 
 echo "starting postgresql service"
+#tail -f /dev/null
 postgres -D /etc/postgresql/13/main
