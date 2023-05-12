c9e4892a53455dca44a69d0ffa728b8c7083b9d1
Zelinsta <theotimeq@gmail.com>
2023-05-12 16:32:54 +0200

    database update bug ?

--- Dockers/Database/tools/start_db.sh
+++ Dockers/Database/tools/start_db.sh
@@ -13,36 +13,17 @@ chmod 750 /var/lib/postgresql/13/main
 echo "adding postgres command to path"
 export PATH+=:/usr/lib/postgresql/13/bin
 
-<<<<<<< HEAD
-if [ psql -l | grep crunchy_db ]; then
-	echo "Database exists"
-else
-
-	echo "No database found, creating one"
-=======
 echo "No database found, creating one"
->>>>>>> cd6c7e3 (database update bug ?)
 
-	pg_ctlcluster 13 main start
+pg_ctlcluster 13 main start
 
-	echo "loading migrate.sql..."
-	psql -U postgres -d postgres -a -f migrate.sql
+echo "loading migrate.sql..."
+psql -U postgres -d postgres -a -f migrate.sql
 
-	echo "migrate.sql loaded!"
+echo "migrate.sql loaded!"
 
-	pg_ctlcluster 13 main stop
+pg_ctlcluster 13 main stop
 
-<<<<<<< HEAD
-	echo "add addresses in configuration files"
-	sed -i "s/port = .*/port = $DATA_BASE_PORT/" /etc/postgresql/13/main/postgresql.conf
-	sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" /etc/postgresql/13/main/postgresql.conf
-
-	echo "host  all  all 0.0.0.0/0 md5" >> /etc/postgresql/13/main/pg_hba.conf
-
-fi
-
-chmod 777 /var/lib/postgresql/13
-=======
 sed -i "s/port = .*/port = $DATA_BASE_PORT/" /etc/postgresql/13/main/postgresql.conf
 sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" /etc/postgresql/13/main/postgresql.conf
 echo "host  all  all 0.0.0.0/0 md5" >> /etc/postgresql/13/main/pg_hba.conf
@@ -53,7 +34,6 @@ service postgresql stop
 sed -i "s/port = .*/port = $DATA_BASE_PORT/" /etc/postgresql/13/main/postgresql.conf
 sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" /etc/postgresql/13/main/postgresql.conf
 echo "host  all  all 0.0.0.0/0 md5" >> /etc/postgresql/13/main/pg_hba.conf
->>>>>>> cd6c7e3 (database update bug ?)
 
 echo "starting postgresql service"
 postgres -D /etc/postgresql/13/main
