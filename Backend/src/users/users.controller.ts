1ed37e33c9d0ce3434da7f5df0938c2a296db0d7
bperriol <bperriol@bperriol.42.fr.bperriol.42.fr>
2023-05-15 13:56:01 +0200

    user controller

--- Backend/src/users/users.controller.ts
+++ Backend/src/users/users.controller.ts
@@ -2,27 +2,8 @@ import { Body, Controller, Get, Post, UsePipes, ValidationPipe } from '@nestjs/c
 import { UsersService } from './users.service';
 import { createUserDto } from './dto/CreateUser.dto';
 
-// @Controller('users')
-// export class UsersController {
-
-// 	constructor(private readonly usersService: UsersService) {};	
-
-// 	@Get()
-// 	findAll() {
-// 		console.log('get route');
-// 		return this.usersService.findAll();
-// 	}
-
-// 	@Post()
-// 	@UsePipes(ValidationPipe)
-// 	addUser(@Body() createUserDto: createUserDto) {
-// 		console.log('post route');
-// 		return this.usersService.addUser(createUserDto);
-// 	}
-// }
-
 @Controller('api/users')
-export class UserController {
+export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post('add')
--- Backend/src/users/users.module.ts
+++ Backend/src/users/users.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
-import { UserController, UsersController } from './users.controller';
-import { UsersService, UserService } from './users.service';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/typeorm/User.entity';
 
 @Module({
 	imports: [TypeOrmModule.forFeature([User])],
-	controllers: [UsersController, UserController],
-	providers: [UsersService, UserService]
+	controllers: [UsersController],
+	providers: [UsersService]
 })
 export class UsersModule {}
