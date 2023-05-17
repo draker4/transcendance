1f2020e00623e197db149a6ee43ab2c9fb9363a5
bperriol <bperriol@bperriol.42.fr.bperriol.42.fr>
2023-05-17 15:03:51 +0200

    database ok

--- Dockers/Database/tools/start_db.sh
+++ Dockers/Database/tools/start_db.sh
@@ -2,15 +2,6 @@
 
 echo "Begin Database configuration"
 
-# Change les variable dans le script d'initialisation de la db
-# sed -i "s/DATA_BASE_NAME/$DATA_BASE_NAME/g" ./migrate.sql
-# sed -i "s/DATA_BASE_PASSWORD/$DATA_BASE_PASSWORD/g" ./migrate.sql
-# sed -i "s/DATA_BASE_USER/$DATA_BASE_USER/g" ./migrate.sql
-
-#mkdir -p /etc/postgresql/13/
-#mkdir -p /var/lib/postgresql/13/
-#chmod 777 /var/lib/postgresql/13/
-
 echo "---------------Adding postgres command to path----------"
 export PATH+=:/usr/lib/postgresql/13/bin
 
@@ -18,20 +9,12 @@ export PATH+=:/usr/lib/postgresql/13/bin
 if [ ! -d "/etc/postgresql/13/conf_done" ]; then
     echo "---------No database found, creating one------------"
 
-    #On change la ou s'enregistre les fichier de la bs , idealement dans le volume....
-    #mkdir -p /etc/postgresql/13/data
-    #initdb /etc/postgresql/13/data
-    #touch /etc/postgresql/13/main/postgresql.auto.conf
-    #sed -i "s|data_directory = '/var/lib/postgresql/13/main'|data_directory = '/etc/postgresql/13/main'|" /etc/postgresql/13/main/postgresql.conf   
-    
     sed -i "s/port = .*/port = $DATA_BASE_PORT/" /etc/postgresql/13/main/postgresql.conf
     sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" /etc/postgresql/13/main/postgresql.conf
     echo "host  all  all 0.0.0.0/0 md5" >> /etc/postgresql/13/main/pg_hba.conf
 
     pg_ctlcluster 13 main start
 
-    #echo "---------------Loading migrate.sql...---------------"
-    #psql -U postgres -d postgres -a -f migrate.sql
     echo "---------------Creating user and database...---------------"
     createuser $DATA_BASE_USER
     createdb $DATA_BASE_NAME -O $DATA_BASE_USER
@@ -45,9 +28,6 @@ if [ ! -d "/etc/postgresql/13/conf_done" ]; then
 
     #On indique que la db est configuré
     touch /etc/postgresql/13/conf_done
-
-    #service postgresql restart
-    #service postgresql stop
 fi
 
 echo "---------------Starting postgresql service---------------"
