d6c3c5221866b1143c96043ada25b10c5362f846
Loup <loumarti@student.42lyon.fr>
2023-06-02 14:40:03 +0200

    added input

--- Frontend/transcendence-app/src/components/LoupTests/ProfileCard.tsx
+++ Frontend/transcendence-app/src/components/LoupTests/ProfileCard.tsx
@@ -3,18 +3,37 @@
 import Profile from "@/services/Profile.service";
 import styles from "@/styles/LoupTest/ProfileCard.module.css"
 import ButtonChangeAvatar from "./ButtonChangeAvatar";
-import { useState } from "react";
+import { useState, Dispatch, SetStateAction, ChangeEvent } from "react";
 
 
 type Props = {
     profile: Profile;
 }
 
+type AvatarState = {
+    avatar: string;
+    setAvatar: Dispatch<SetStateAction<string>>;
+    profileImage: string;
+}
+
+
 export default function ProfileCard({profile} : Props) {
 
     const [avatar, setAvatar] = useState<string>(profile.image);
 
     const avatarAltValue: string = `player ${profile.login}'s avatar`;
+
+    const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0];
+        const reader = new FileReader();
+
+        reader.onload = (e) => {
+            setAvatar(e.target?.result as string);
+        };
+
+        if (file !== undefined)
+            reader.readAsDataURL(file);
+    }
 
     const content = (
         
@@ -24,6 +43,7 @@ export default function ProfileCard({profile} : Props) {
                 <img className={styles.img} src={avatar} alt={avatarAltValue}/>
             </div>
             <ButtonChangeAvatar avatar={avatar} setAvatar={setAvatar} profileImage={profile.image}/>
+            <input className={styles.input} type="file" onChange={handleFileChange} accept="image/*" />
         </div>
 
     );
