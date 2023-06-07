00c77b6f6f0b7b26fdc111c650775758c0b669a8
Loup Martineau <loumarti@z1r10p3.42lyon.fr>
2023-06-07 15:59:07 +0200

    profile wip

--- Frontend/transcendence-app/src/components/logged-in/profile/avatar/ProfileLogin.tsx
+++ Frontend/transcendence-app/src/components/logged-in/profile/avatar/ProfileLogin.tsx
@@ -1,14 +1,20 @@
 import styles from "@/styles/profile/AvatarCard.module.css"
 import Profile from "@/services/Profile.service";
+import EditButton from "./EditButton";
 
 type Props = {
     profile: Profile;
 }
 
 export default function ProfileLogin({profile} : Props) {
-  return (
-	<div className={styles.login}>
-		<h1>{profile.login}</h1>
+	const isProfilOwner = true;
+	
+	return (
+	<div className={styles.loginCard}>
+		<div className={styles.login}>
+			<h1>{profile.login}</h1>
+		</div>
+		{isProfilOwner && <EditButton />}
 	</div>
   )
 }
