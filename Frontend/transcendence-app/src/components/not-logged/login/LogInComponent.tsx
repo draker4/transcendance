872d3ba458ab4b8f689be0794abb66e50cfb6e18
draker4 <baptiste.perriolat@live.fr>
2023-06-07 13:25:54 +0200

    colors

--- Frontend/transcendence-app/src/components/not-logged/login/LogInComponent.tsx
+++ Frontend/transcendence-app/src/components/not-logged/login/LogInComponent.tsx
@@ -1,5 +1,5 @@
 import registerForm from "@/lib/auth/registerForm";
-import styles from "@/styles/SignUp.module.css"
+import styles from "@/styles/Login.module.css"
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import { useGoogleReCaptcha } from "react-google-recaptcha-v3";
@@ -8,9 +8,11 @@ export default function LogInComponent() {
 	
 	const	router = useRouter();
 	const	[emailUsed, setEmailUsed] = useState<boolean>(false);
-	const	[loginUsed, setLoginUsed] = useState<string>("");
+	// const	[loginUsed, setLoginUsed] = useState<string>("");
+	const	[password, setPassword] = useState<boolean>(false);
 	const	[passwordSecured, setPasswordSecured] = useState<string>("");
 	const	[register, setRegister] = useState<boolean>(false);
+	const	[title, setTitle] = useState<string>("Connection / Inscription");
 	const	[notif, setNotif] = useState<string>("");
 	const	{ executeRecaptcha } = useGoogleReCaptcha();
 
@@ -29,13 +31,14 @@ export default function LogInComponent() {
 
 	const	signin = (e: React.MouseEvent<HTMLElement>) => {
 		e.preventDefault();
-		router.push("/welcome/signin");
+		// router.push("/welcome/signin");
 	}
 
 	return (
 		<div className={styles.main}>
+			<p className={styles.title}>{ title }</p>
+			<p>Enter your email to log in or register your account</p>
 			<div className={styles.box}>
-				<p className={styles.welcome}>Welcome</p>
 
 				<form action={async (data: FormData) => {
 					if (!executeRecaptcha) {
@@ -66,13 +69,13 @@ export default function LogInComponent() {
 					
 					const	res: {
 						checkEmail: boolean,
-						checkLogin: string,
+						// checkLogin: string,
 						passwordSecured: string,
 						register: boolean,
 					} = await registerForm(data);
 					
 					setEmailUsed(res.checkEmail);
-					setLoginUsed(res.checkLogin);
+					// setLoginUsed(res.checkLogin);
 					setPasswordSecured(res.passwordSecured);
 					setRegister(res.register);
 				}
@@ -82,22 +85,28 @@ export default function LogInComponent() {
 				}
 
 				}} className={styles.form}>
-					<input type="text" aria-label="Username" autoComplete="username" placeholder="login" name="login" className={styles.input} required/>
-					{ loginUsed.length > 0 && <div>{ loginUsed }!</div> }
+					{/* <input type="text" aria-label="Username" autoComplete="username" placeholder="login" name="login" className={styles.input} required/> */}
+					{/* { loginUsed.length > 0 && <div>{ loginUsed }!</div> } */}
 					<input type="email" autoComplete="username" placeholder="email" name="email" className={styles.input} required/>
 					{ emailUsed && <div>Email already used !</div> }
-					<input type="password" autoComplete="new-password" placeholder="password" name="password" className={styles.input} required/>
-					{ passwordSecured.length > 0 && <div>{ passwordSecured }</div> }
+					{ password &&
+						<div>
+							<input type="password" autoComplete="new-password" placeholder="password" name="password" className={styles.input} required/>
+							{ passwordSecured.length > 0 && <div>{ passwordSecured }</div> }
+						</div>
+					}
 					{ notif.length > 0 && <div>{ notif }</div>}
 					<button type="submit" className={styles.continue}>Continue</button>
 				</form>
 
-				<p className={styles.signUp}>Already have an account? <span onClick={signin}>Sign in</span></p>
-				<p className={styles.or}>Or</p>
-				<div className={styles.buttons}>
+				<p className={styles.or}>----- Or -----</p>
+				<div className={styles.logButtons}>
+					<img src="/images/auth/42_Logo.png" onClick={open42} className={styles.logImg}/>
+				</div>
+				{/* <div className={styles.buttons}>
 					<button onClick={open42} className={styles.buttonLogin}>Login with 42</button>
 					<button onClick={openGoogle} className={styles.buttonLogin}>Login with Google</button>
-				</div>
+				</div> */}
 			</div>
 		</div>
 	);
--- Frontend/transcendence-app/src/lib/auth/registerUser.ts
+++ Frontend/transcendence-app/src/lib/auth/registerUser.ts
@@ -1,10 +1,10 @@
-export default async function registerUser(email: string, login: string, password: string) {
+export default async function registerUser(email: string, password: string) {
 	const	response = await fetch("http://backend:4000/api/auth/register", {
 		method: "POST",
 		headers: {"Content-Type": "application/json"},
 		body: JSON.stringify({
 			email: email,
-			login: login,
+			// login: login,
 			password: password,
 			provider: "email",
 		}),
