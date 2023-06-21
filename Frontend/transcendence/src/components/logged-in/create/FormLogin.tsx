be5d5b2902068acd4ae093d270a39e2445e4394f
Baptiste <bapboisson@gmail.com>
2023-06-21 12:39:48 +0200

    bug fix

--- Frontend/transcendence/src/components/logged-in/create/FormLogin.tsx
+++ Frontend/transcendence/src/components/logged-in/create/FormLogin.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { checkLoginFormat } from "@/lib/create/checkLogin";
 import ChooseAvatar from "./ChooseAvatar";
@@ -8,129 +8,124 @@ import { handleActionServer } from "@/lib/create/handleActionServer";
 import { useRouter } from "next/navigation";
 import avatarType from "@/types/Avatar.type";
 
-export default function FormLogin({ avatars, token }: {
-	avatars: string[],
-	token: string,
+export default function FormLogin({
+  avatars,
+  token,
+}: {
+  avatars: string[];
+  token: string;
 }) {
+  const [notif, setNotif] = useState<string>("");
+  const [text, setText] = useState<string>("");
+  const [access_token, setToken] = useState<string>("");
+  const router = useRouter();
+  const avatarChosenRef = useRef<avatarType>({
+    image: "",
+    variant: "circular",
+    borderColor: "var(--accent)",
+    backgroundColor: "var(--accent)",
+    text: "",
+    empty: true,
+    login: "",
+  });
 
-	const	[notif, setNotif] = useState<string>("");
-	const	[text, setText] = useState<string>("");
-	const	[access_token, setToken] = useState<string>("");
-	const	router = useRouter();
-	const	avatarChosenRef = useRef<avatarType>({
-		image: "",
-		variant: "circular",
-		borderColor: "var(--accent)",
-		backgroundColor: "var(--accent)",
-		text: "",
-		empty: true,
-		login: "",
-	});
+  const selectAvatar = (avatar: avatarType) => {
+    avatar.login = avatarChosenRef.current.login;
+    avatarChosenRef.current = avatar;
+  };
 
-	const	selectAvatar = (avatar: avatarType) => {
-		avatar.login = avatarChosenRef.current.login;
-		avatarChosenRef.current = avatar;
-	}
+  const borderColor = (color: string) => {
+    avatarChosenRef.current.borderColor = color;
+  };
 
-	const	borderColor = (color: string) => {
-		avatarChosenRef.current.borderColor = color;
-	}
+  const backgroundColor = (color: string) => {
+    avatarChosenRef.current.backgroundColor = color;
+  };
 
-	const	backgroundColor = (color: string) => {
-		avatarChosenRef.current.backgroundColor = color;
-	}
+  const handleText = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const text = e.target.value;
 
-	const	handleText = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const	text = e.target.value;
+    avatarChosenRef.current.text = text.toUpperCase().slice(0, 2);
+    avatarChosenRef.current.login = text;
+    setText(text.toUpperCase().slice(0, 2));
+  };
 
-		avatarChosenRef.current.text = text.toUpperCase().slice(0, 2);
-		avatarChosenRef.current.login = text;
-		setText(text.toUpperCase().slice(0, 2));
-	}
+  useEffect(() => {
+    const changeCookie = async () => {
+      try {
+        const res = await fetch("http://localhost:3000/api/login", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            access_token,
+          }),
+        });
 
-	useEffect(() => {
+        const data = await res.json();
 
-		const changeCookie = async () => {
-			try {
-				const	res = await fetch("http://localhost:3000/api/login", {
-					method: "POST",
-					headers: { "Content-Type": "application/json" },
-					body: JSON.stringify({
-						access_token,
-					}),
-				});
+        if (!res.ok || data.error) throw new Error();
 
-				const	data = await res.json();
+        router.refresh();
+      } catch (error) {
+        console.log(error);
+        setNotif("Something went wrong, please try again");
+      }
+    };
 
-				if (!res.ok || data.error)
-					throw new Error();
-				
-				router.refresh();
-			}
-			catch (error) {
-				console.log(error);
-				setNotif("Something went wrong, please try again");
-			}
+    if (access_token.length > 0) {
+      changeCookie();
+    }
+  }, [access_token, router]);
 
-		}
+  const handleActionLogin = async (data: FormData) => {
+    const loginUser = data.get("login") as string;
+    const loginChecked = checkLoginFormat(loginUser);
+    setNotif(loginChecked);
 
-		if (access_token.length > 0) {
-			changeCookie();
-		}
-	}, [access_token])
-	
-	const	handleActionLogin = async (data: FormData) => {
-		const	loginUser = data.get('login') as string;
-		const	loginChecked = checkLoginFormat(loginUser);
-		setNotif(loginChecked);
+    if (loginChecked.length > 0) return;
 
-		if (loginChecked.length > 0)
-			return ;
-		
-		const	res: {
-			exists: string,
-			token: string,
-		} = await handleActionServer(loginUser, avatarChosenRef.current, token);
+    const res: {
+      exists: string;
+      token: string;
+    } = await handleActionServer(loginUser, avatarChosenRef.current, token);
 
-		setNotif(res.exists);
-		setToken(res.token);
-	}
+    setNotif(res.exists);
+    setToken(res.token);
+  };
 
-	return (
-		<div className={styles.main}>
-			<h3>You're almost there! 😁</h3>
+  return (
+    <div className={styles.main}>
+      <h3>You are almost there! 😁</h3>
 
-			<form action={handleActionLogin}>
-			<form>
-				<label>
-					Please choose your login!
-				</label>
-				<p className={styles.little}>Don't worry, you can change it later.</p>
-				
-				<input
-					type="text"
-					name="login"
-					placeholder="login"
-					maxLength={10}
-					onChange={handleText}
-				/>
+      <form action={handleActionLogin}>
+        <label>Please choose your login!</label>
+        <p className={styles.little}>Do not worry, you can change it later.</p>
 
-				{notif.length > 0 && <div className={styles.notif}>{notif}</div>}
-				<p className={styles.choose}>Make you pretty:</p>
-				
-				<div className={styles.avatars}>
-					<ChooseAvatar
-						selectBorder={borderColor}
-						selectBackground={backgroundColor}
-						selectAvatar={selectAvatar}
-						text={text}
-						avatars={avatars}
-					/>
-				</div>
+        <input
+          type="text"
+          name="login"
+          placeholder="login"
+          maxLength={10}
+          onChange={handleText}
+        />
 
-				<button type="button" title="Create avatar">Let's go!</button>
-			</form>
-		</div>
-	);
+        {notif.length > 0 && <div className={styles.notif}>{notif}</div>}
+        <p className={styles.choose}>Make you pretty:</p>
+
+        <div className={styles.avatars}>
+          <ChooseAvatar
+            selectBorder={borderColor}
+            selectBackground={backgroundColor}
+            selectAvatar={selectAvatar}
+            text={text}
+            avatars={avatars}
+          />
+        </div>
+
+        <button type="button" title="Create avatar">
+          Let is go!
+        </button>
+      </form>
+    </div>
+  );
 }
-
