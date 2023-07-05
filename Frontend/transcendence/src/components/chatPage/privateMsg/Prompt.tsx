3cde692ed257288005f9d0251cda46e25a29c25b
Loup <loumarti@student.42lyon.fr>
2023-07-05 18:10:39 +0200

    prompt priv msg is ok for now

--- Frontend/transcendence/src/components/chatPage/privateMsg/Prompt.tsx
+++ Frontend/transcendence/src/components/chatPage/privateMsg/Prompt.tsx
@@ -1,35 +1,78 @@
 import styles from "@/styles/chatPage/privateMsg/ChatPrivateMsg.module.css";
 import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
+import { useEffect, useRef, useState } from "react";
 
 type Props = {
   pongie: Pongie;
 };
 
-
-
 export default function Prompt({ pongie }: Props) {
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const [isEmpty, setIsEmpty] = useState<boolean>(true);
+  const [text, setText] = useState<string>("");
 
-const placeholder = "send a message at " + pongie.login;
+  useEffect(() => {
+    adjustHeight();
+    if (text.length === 1 && text[0] === "\n") {
+      setText("");
+      setIsEmpty(true);
+      if (textareaRef.current) textareaRef.current.style.height = "40px";
+    }
+  }, [text]);
 
-const handleSendMsg = (e: any) => {
-  e.preventDefault();
-  console.log("paper plane pushed !");
-}
+  const placeholder = "Send a message at " + pongie.login;
+  const buttonAddedClass = isEmpty
+    ? styles.disabledButton
+    : styles.activeButton;
 
+  const handleSendMsg = (e: React.FormEvent | null) => {
+    e?.preventDefault();
+
+    console.log("Message to send :", text);
+    setText("");
+    setIsEmpty(true);
+  };
+
+  const handleTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setText(e.target.value);
+    setIsEmpty(e.target.value === "");
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === "Enter" && !e.shiftKey) {
+      handleSendMsg(null);
+    }
+  };
+
+  const adjustHeight = () => {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = `${Math.min(
+        textareaRef.current.scrollHeight,
+        600
+      )}px`;
+    }
+  };
 
   return (
-	<div className={styles.prompt}>
-    <div className={styles.promptCard}>
+    <div className={styles.prompt}>
       <form onSubmit={handleSendMsg}>
-        <textarea name="prompt"
-         placeholder={placeholder} />
-         <button className={styles.paperPlane} type="submit">
-         <FontAwesomeIcon icon={faPaperPlane} />
-         </button>
+        <textarea
+          name="prompt"
+          ref={textareaRef}
+          placeholder={placeholder}
+          onChange={handleTextChange}
+          onKeyDown={handleKeyDown}
+          value={text}
+          maxLength={350}
+        />
+        <button
+          className={`${styles.paperPlane} ${buttonAddedClass}`}
+          type="submit"
+        >
+          <FontAwesomeIcon icon={faPaperPlane} />
+        </button>
       </form>
     </div>
-  </div>
-  )
+  );
 }
