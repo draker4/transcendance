b0c9ecd090e48cc2f97431e544cb1753335ba536
Loup <loumarti@student.42lyon.fr>
2023-07-06 17:58:50 +0200

    CSS on chat>MessageBoard

--- Frontend/transcendence/src/components/chatPage/privateMsg/ChatPrivateMsg.tsx
+++ Frontend/transcendence/src/components/chatPage/privateMsg/ChatPrivateMsg.tsx
@@ -57,6 +57,172 @@ export default function ChatPrivateMsg( { icon, pongie, myself }: Props) {
       sender: pongie,
       date: new Date(),
     },
+
+    {
+      content: "When I find myself in times of trouble"
+      + "Pikachu comes to me"
+      + "Speaking words of wisdom, let it be"
+      + "And in my hour of darkness"
+      + "Charizard is standing right in front of me"
+      + "Whispering words of wisdom, let it be",
+      sender: me,
+      date: new Date(),
+    },
+
+    {
+      content:
+      + "Let it be, let it be"
+      + "Let it be, let it be"
+      + "Whisper words of wisdom, let it be",
+      sender: me,
+      date: new Date(),
+    },
+      
+      {
+        content:
+      + "And when the night is cloudy"
+      + "Jigglypuff starts to sing"
+      + "Soothing melodies, let it be"
+      + "Oh, when the broken-hearted people"
+      + "Find solace in the sea"
+      + "Vaporeon shows us all, let it be",
+      sender: me,
+      date: new Date(),
+    },
+      
+      {
+        content:
+      + "Let it be, let it be"
+      + "Let it be, let it be"
+      + "Find solace in the sea, let it be",
+      sender: me,
+      date: new Date(),
+    },
+      
+      {
+        content:
+      + "And when the morning sunlight"
+      + "Reflects off Butterfree's wings"
+      + "We know that love will guide the way, let it be"
+      + "For though they may be parted"
+      + "Espeon and Umbreon still shine their light"
+      + "Leading us to peace, let it be",
+      sender: me,
+      date: new Date(),
+    },
+
+      {
+        content:
+      + "Let it be, let it be"
+      + "Let it be, let it be"
+      + "Shining light of peace, let it be",
+      sender: me,
+      date: new Date(),
+    },
+      
+      {
+        content:
+      + "Let it be, let it be"
+      + "Let it be, let it be"
+      + "Whisper words of wisdom, let it be",
+      sender: me,
+      date: new Date(),
+    },
+
+    {
+      content: "Bonjour cher ami",
+      sender: pongie,
+      date: new Date(),
+    },
+
+    {
+      content: "Yop ! on se fait une game ?",
+      sender: me,
+      date: new Date(),
+    },
+    
+    {
+      content: "J'ai trop envie de cruncher !",
+      sender: me,
+      date: new Date(),
+    },
+
+    {
+      content: "Allez, let's Pong !",
+      sender: pongie,
+      date: new Date(),
+    },
+
+    {
+      content: "When I find myself in times of trouble"
+      + "Pikachu comes to me"
+      + "Speaking words of wisdom, let it be"
+      + "And in my hour of darkness"
+      + "Charizard is standing right in front of me"
+      + "Whispering words of wisdom, let it be",
+      sender: me,
+      date: new Date(),
+    },
+
+    {
+      content:
+      + "Let it be, let it be"
+      + "Let it be, let it be"
+      + "Whisper words of wisdom, let it be",
+      sender: me,
+      date: new Date(),
+    },
+      
+      {
+        content:
+      + "And when the night is cloudy"
+      + "Jigglypuff starts to sing"
+      + "Soothing melodies, let it be"
+      + "Oh, when the broken-hearted people"
+      + "Find solace in the sea"
+      + "Vaporeon shows us all, let it be",
+      sender: me,
+      date: new Date(),
+    },
+      
+      {
+        content:
+      + "Let it be, let it be"
+      + "Let it be, let it be"
+      + "Find solace in the sea, let it be",
+      sender: me,
+      date: new Date(),
+    },
+      
+      {
+        content:
+      + "And when the morning sunlight"
+      + "Reflects off Butterfree's wings"
+      + "We know that love will guide the way, let it be"
+      + "For though they may be parted"
+      + "Espeon and Umbreon still shine their light"
+      + "Leading us to peace, let it be",
+      sender: me,
+      date: new Date(),
+    },
+
+      {
+        content:
+      + "Let it be, let it be"
+      + "Let it be, let it be"
+      + "Shining light of peace, let it be",
+      sender: me,
+      date: new Date(),
+    },
+      
+      {
+        content:
+      + "Let it be, let it be"
+      + "Let it be, let it be"
+      + "Whisper words of wisdom, let it be",
+      sender: me,
+      date: new Date(),
+    },
   ];
 
   const [messages, setMessages] = useState<PrivateMsgType[]>(messagesExample);
--- Frontend/transcendence/src/components/chatPage/privateMsg/MessageItem.tsx
+++ Frontend/transcendence/src/components/chatPage/privateMsg/MessageItem.tsx
@@ -1,38 +1,49 @@
 import styles from "@/styles/chatPage/privateMsg/ChatPrivateMsg.module.css";
 import AvatarUser from "@/components/loggedIn/avatarUser/AvatarUser";
-  
+
 type GroupedPrivateMsgType = {
-    sender: Pongie;
-    date: Date;
-    messages: PrivateMsgType[];
-  };
+  sender: Pongie;
+  date: Date;
+  messages: PrivateMsgType[];
+};
 
-  type Props = {
-    groupedMessages: GroupedPrivateMsgType;
-  };
+type Props = {
+  groupedMessages: GroupedPrivateMsgType;
+};
 
-export default function MessageItem( {groupedMessages} : Props) {
-
-    const mappingMessages = groupedMessages.messages.map((msg, index) => (
-        <p key={index}>{msg.content}</p>
-    ));
+export default function MessageItem({ groupedMessages }: Props) {
+  const mappingMessages = groupedMessages.messages.map((msg, index) => (
+    <p key={index}>{msg.content}</p>
+  ));
 
   return (
-    <div>
-         {/* <AvatarUser
-        avatar={message.sender.avatar}
-        borderSize="2px"
-        borderColor={message.sender.avatar.borderColor}
-        backgroundColor={message.sender.avatar.backgroundColor}
-      /> */}
-      <h2>{groupedMessages.sender.login}</h2>
-      <h6>{`at ${groupedMessages.date.getSeconds()}`}</h6>
-      {mappingMessages}
+    <div className={styles.msgItem}>
+      {/* Avatar Side */}
+      <div className={styles.avatarSide}>
+        <div className={styles.avatarSlot}>
+          <AvatarUser
+            avatar={groupedMessages.sender.avatar}
+            borderSize="3px"
+            borderColor={groupedMessages.sender.avatar.borderColor}
+            backgroundColor={groupedMessages.sender.avatar.backgroundColor}
+          />
+        </div>
+      </div>
+
+      {/* Text Side */}
+      <div className={styles.textSide}>
+        {/* Header */}
+        <div className={styles.tsheader}>
+          <div className={styles.name} style={{color: groupedMessages.sender.avatar.borderColor}}>
+            {groupedMessages.sender.login}
+          </div>
+          <div className={styles.date}>
+            {`at ${groupedMessages.date.getSeconds()}`}
+          </div>
+        </div>
+        {/* Content */}
+        <div className={styles.content}>{mappingMessages}</div>
+      </div>
     </div>
-  )
-
-    // return (
-    //     <div className={styles.msgItem}></div>
-
-    // )
+  );
 }
