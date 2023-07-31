a86b7f5d712ea8b32c89aa03e6a8f947a60c2b0a
Baptiste <bapboisson@gmail.com>
2023-07-31 08:00:41 +0200

    error uuid

--- Dockers/Backend/tools/start_nest.sh
+++ Dockers/Backend/tools/start_nest.sh
@@ -3,11 +3,18 @@ echo "Begin NestJS configuration"
 
 echo "Install required depedencies"
 cd home
-
 yarn global add node-gyp
 yarn cache clean
 yarn install --check-files
 
+echo "Update depedencies"
+yarn upgrade
+
 #Start dev env
 echo "Starting dev frontend environment"
 yarn run start:dev:backend
+
+#Start prod env
+#echo "Starting prod frontend environment"
+#yarn run start:prod:backend
+
--- Dockers/Frontend/tools/start_next.sh
+++ Dockers/Frontend/tools/start_next.sh
@@ -7,6 +7,13 @@ yarn global add node-gyp
 yarn cache clean
 yarn install --check-files
 
+echo "Update depedencies"
+yarn upgrade
+
 #Start dev env
 echo "Starting dev frontend environment"
 yarn run start:dev:frontend
+
+#Start prod env
+#echo "Starting prod frontend environment"
+#yarn run start:prod:frontend
