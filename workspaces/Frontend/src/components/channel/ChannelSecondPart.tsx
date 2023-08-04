111e10b294ac82de71915b09b70286c8430849ab
Loup Martineau <loumarti@z1r9p2.42lyon.fr>
2023-08-04 10:27:58 +0200

    startUp add chat socket in channel profile

--- workspaces/Frontend/src/components/channel/ChannelSecondPart.tsx
+++ workspaces/Frontend/src/components/channel/ChannelSecondPart.tsx
@@ -1,4 +1,5 @@
 import styles from "@/styles/profile/Profile.module.css";
+import { cookies } from "next/headers";
 import ChannelInfoCard from "./infos/ChannelInfoCard";
 
 type Props = {
@@ -8,9 +9,15 @@ type Props = {
 
 export default function ChannelSecondPart({ channelAndUsersRelation, myRelation }: Props) {
 
+	const token = cookies().get("crunchy-token")?.value;
+
+	// [+][!] ameliorer le composant d'erreure
+    if (!token)
+		return (<div> E R R O R ... try refresh and reconnect</div>);
+
   return (
 	<div className={styles.both + " " + styles.second}>
-		<ChannelInfoCard channelAndUsersRelation={channelAndUsersRelation} myRelation={myRelation} />
+		<ChannelInfoCard token={token} channelAndUsersRelation={channelAndUsersRelation} myRelation={myRelation} />
 	</div>
   )
 }
--- workspaces/Frontend/src/components/channel/infos/ChannelInfoCard.tsx
+++ workspaces/Frontend/src/components/channel/infos/ChannelInfoCard.tsx
@@ -1,20 +1,65 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "@/styles/profile/InfoCard.module.css";
 import NavbarChannelInfo from "./NavbarChannelInfo";
 import SectionPongers from "./sections/SectionPongers";
+import { Socket } from "socket.io-client";
+import disconnect from "@/lib/disconnect/disconnect";
+import { useRouter } from "next/navigation";
+import ChatService from "@/services/Chat.service";
 
 type Props = {
+  token:string;
   channelAndUsersRelation: ChannelUsersRelation;
   myRelation: UserRelation;
 };
 
 export default function ChannelInfoCard({
+  token,
   channelAndUsersRelation,
   myRelation,
 }: Props) {
   const [activeButton, setActiveButton] = useState(0);
+  const [socket, setSocket] = useState<Socket | undefined>(undefined);
+  const router = useRouter();
+
+  // Chat Websocket call
+  useEffect(() => {
+
+	const handleError = () => {
+		setSocket(undefined);
+	  }
+
+	  const	disconnectClient = async () => {
+		await disconnect();
+		router.refresh();
+	}
+
+	  if (!socket) {
+		const intervalId = setInterval(() => {
+		  const chatService = new ChatService(token);
+		  
+				  if (chatService.disconnectClient) {
+					  clearInterval(intervalId);
+					  disconnectClient();
+				  }
+  
+		  if (chatService.socket) {
+			setSocket(chatService.socket);
+			clearInterval(intervalId);
+		  }
+		}, 500);
+	  }
+
+	  socket?.on("disconnect", handleError);
+
+	  return () => {
+		socket?.off("disconnect", handleError);
+	  }
+
+  }, [socket]);
+
 
   return (
     <>
