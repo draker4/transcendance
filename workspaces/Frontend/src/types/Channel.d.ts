952490003f399824a0b0549ef3e70fef283fa1f6
draker4 <baptiste.perriolat@live.fr>
2023-08-09 16:39:15 +0200

    ok

--- workspaces/Frontend/src/types/Channel.d.ts
+++ workspaces/Frontend/src/types/Channel.d.ts
@@ -1,9 +1,12 @@
 type	Channel = {
-	id: number,
-	name: string,
-	avatar: Avatar,
-	type: 'public' | 'protected' | 'private' | 'privateMsg',
-	joined: boolean;
+	id: number;
+	name: string;
+	avatar: Avatar;
+	type: 'public' | 'protected' | 'private' | 'privateMsg';
+	isChanOp:boolean;
+	joined:boolean;
+	invited:boolean;
+	banned:boolean;
 	lastMessage?: {
 		user: User;
 		content: string;
