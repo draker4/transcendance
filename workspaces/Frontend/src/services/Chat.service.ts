45027f969c58aa03b167a1fef1090084b64573dd
Baptiste <bapboisson@gmail.com>
2023-09-20 17:54:18 +0200

    recherche

--- workspaces/Frontend/src/services/Chat.service.ts
+++ workspaces/Frontend/src/services/Chat.service.ts
@@ -1,137 +1,142 @@
 import { Socket, io } from "socket.io-client";
 
 export default class ChatService {
-	public static instance: ChatService | null = null;
+  public static instance: ChatService | null = null;
 
-	public	socket: Socket | undefined = undefined;
-	public	disconnectClient: boolean = false;
-	public	loading: boolean = false;
-	public	nbExceptions: number = 0;
+  public socket: Socket | undefined = undefined;
+  public disconnectClient: boolean = false;
+  public loading: boolean = false;
+  public nbExceptions: number = 0;
 
-	// Singleton
-    constructor(token?: string) {
-		
-        if (ChatService.instance) {
-			return ChatService.instance;
-		}
-		
-		if (token) {
-			this.initializeSocket(token);
-			ChatService.instance = this;
-		}
-		else {
-			this.disconnect();
-		}
+  // Singleton
+  constructor(token?: string) {
+    if (ChatService.instance) {
+      return ChatService.instance;
     }
 
-	// Create socket + listen errors & exceptions
-	public initializeSocket(token: string) {
+    if (token) {
+      this.initializeSocket(token);
+      ChatService.instance = this;
+    } else {
+      this.disconnect();
+    }
+  }
 
-		while (this.loading) {}
+  // Create socket + listen errors & exceptions
+  public initializeSocket(token: string) {
+    while (this.loading) {}
 
-		if (this.socket)
-			return ;
+    if (this.socket) return;
 
-		this.loading = true;
+    this.loading = true;
 
-		this.socket = io(`ws://${process.env.HOST_IP}:4000/chat`, {
-		  extraHeaders: {
-			Authorization: "Bearer " + token,
-		  },
-		  path: "/chat/socket.io",
-		  reconnection: false,
-		});
+    this.socket = io(`ws://${process.env.HOST_IP}:4000/chat`, {
+      extraHeaders: {
+        Authorization: "Bearer " + token,
+      },
+      path: "/chat/socket.io",
+      reconnection: false,
+    });
 
-		this.socket.on('connect', () => {
-			this.loading = false;
-		});
-		  
-		this.socket.on('disconnect', async () => {
-			this.disconnect();
-		  	await this.refreshSocket();
-		});
-	
-		// Catching error or exception will force disconnect then reconnect
-		this.socket.on("connect_error", async (error: any) => {
-			this.loading = false;
-			if (process.env && process.env.ENVIRONNEMENT && process.env.ENVIRONNEMENT === "dev")
-				console.log("Socket connection error:", error);
-			this.disconnect();
-			await this.refreshSocket();
-		});
-	
-		this.socket.on("error", (error: any) => {
-			if (process.env && process.env.ENVIRONNEMENT && process.env.ENVIRONNEMENT === "dev")
-		  		console.log("Socket error:", error);
-			this.disconnect();
-		});
-	
-		this.socket.on("exception", (exception: any) => {
-			if (process.env && process.env.ENVIRONNEMENT && process.env.ENVIRONNEMENT === "dev")
-		  		console.log("WsException:", exception);
-			if (exception.message === "invalid token")
-				this.disconnectClient = true;
+    this.socket.on("connect", () => {
+      this.loading = false;
+    });
 
-			if (++this.nbExceptions >= 2)
-				this.disconnectClient = true;
+    this.socket.on("disconnect", async () => {
+      this.disconnect();
+      await this.refreshSocket();
+    });
 
-			if (this.nbExceptions === 1)
-				setTimeout(() => {
-					this.nbExceptions = 0;
-				}, 2000);
+    // Catching error or exception will force disconnect then reconnect
+    this.socket.on("connect_error", async (error: any) => {
+      this.loading = false;
+      if (
+        process.env &&
+        process.env.ENVIRONNEMENT &&
+        process.env.ENVIRONNEMENT === "dev"
+      )
+        console.log("Socket connection error:", error);
+      this.disconnect();
+      await this.refreshSocket();
+    });
 
-			this.disconnect();
-		});
-	}
+    this.socket.on("error", (error: any) => {
+      if (
+        process.env &&
+        process.env.ENVIRONNEMENT &&
+        process.env.ENVIRONNEMENT === "dev"
+      )
+        console.log("Socket error:", error);
+      this.disconnect();
+    });
 
-	// Disconnect socket + stop listen errors & exceptions
-	public disconnect() {
-		if (this.socket) {
-			this.socket.off("connect_error");
-			this.socket.off("error");
-			this.socket.off("exception");
-			this.socket.off("refresh");
-			this.socket.disconnect();
-			this.socket = undefined;
-		}
-	}
+    this.socket.on("exception", (exception: any) => {
+      if (
+        process.env &&
+        process.env.ENVIRONNEMENT &&
+        process.env.ENVIRONNEMENT === "dev"
+      )
+        console.log("WsException:", exception);
+      if (exception.message === "invalid token") this.disconnectClient = true;
 
-	private async refreshSocket() {
+      if (++this.nbExceptions >= 2) this.disconnectClient = true;
 
-		if (this.disconnectClient)
-			return ;
+      if (this.nbExceptions === 1)
+        setTimeout(() => {
+          this.nbExceptions = 0;
+        }, 2000);
 
-		try {
-			const res = await fetch(
-				`http://${process.env.HOST_IP}:4000/api/auth/refreshToken`, {
-					method: "POST",
-					credentials: "include",
-				}
-			);
-	
-		  if (!res.ok)
-			throw new Error("fetch failed");
-				  
-		  const data = await res.json()
-	
-		  const resApi = await fetch(`http://${process.env.HOST_IP}:3000/api/auth/setCookies`, {
-			method: "POST",
-			headers: {
-			  "Content-Type": "application/json",
-			},
-			body: JSON.stringify({
-			  accessToken: data.access_token,
-			  refreshToken: data.refresh_token,
-			}),
-		  });
-	
-		  if (!resApi.ok)
-			throw new Error("fetch api failed");
-		  
-		  this.initializeSocket(data.access_token);
-		}
-		catch (error: any) {
-			this.disconnectClient = true;
-		}
-	}
+      this.disconnect();
+    });
+  }
+
+  // Disconnect socket + stop listen errors & exceptions
+  public disconnect() {
+    if (this.socket) {
+      this.socket.off("connect_error");
+      this.socket.off("error");
+      this.socket.off("exception");
+      this.socket.off("refresh");
+      this.socket.disconnect();
+      this.socket = undefined;
+    }
+  }
+
+  private async refreshSocket() {
+    if (this.disconnectClient) return;
+
+    try {
+      const res = await fetch(
+        `http://${process.env.HOST_IP}:4000/api/auth/refreshToken`,
+        {
+          method: "POST",
+          credentials: "include",
+        }
+      );
+
+      if (!res.ok) throw new Error("fetch failed");
+
+      const data = await res.json();
+
+      const resApi = await fetch(
+        `http://${process.env.HOST_IP}:3000/api/auth/setCookies`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            accessToken: data.access_token,
+            refreshToken: data.refresh_token,
+          }),
+        }
+      );
+
+      if (!resApi.ok) throw new Error("fetch api failed");
+
+      this.initializeSocket(data.access_token);
+    } catch (error: any) {
+      this.disconnectClient = true;
+    }
+  }
 }
