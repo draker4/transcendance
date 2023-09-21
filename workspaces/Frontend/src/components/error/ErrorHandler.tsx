02463d8b9549e0fd6cce24fbac9393d92059d56a
Baptiste <bapboisson@gmail.com>
2023-09-21 16:49:37 +0200

    errorHandler

--- workspaces/Frontend/src/components/error/ErrorHandler.tsx
+++ workspaces/Frontend/src/components/error/ErrorHandler.tsx
@@ -1,24 +1,37 @@
-"use client"
+"use client";
 
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 import styles from "@/styles/error/ErrorsMerged.module.css";
 import { faQuestionCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 type Props = {
-    errorTitle?: string;
-    errorNotif?: string;
-    returnLink?: string;
-    returnLinkName?: string;
-  };
+  errorTitle?: string;
+  errorNotif?: string;
+  returnLink?: string;
+  returnLinkName?: string;
+};
 
-
-export default function ErrorHandler({ errorTitle, errorNotif, returnLink, returnLinkName }:Props) {
-
+export default function ErrorHandler({
+  errorTitle,
+  errorNotif,
+  returnLink,
+  returnLinkName,
+}: Props) {
+  const router = useRouter();
   const title = errorTitle ? errorTitle : "Oops, something went wrong";
-  const notif = errorNotif && process.env && process.env.ENVIRONNEMENT && process.env.ENVIRONNEMENT === "dev" ? errorNotif : "Please try again later";
-  const link = returnLink ? returnLink : "/home";
+  const notif =
+    errorNotif &&
+    process.env &&
+    process.env.ENVIRONNEMENT &&
+    process.env.ENVIRONNEMENT === "dev"
+      ? errorNotif
+      : "Please try again later";
   const linkName = returnLinkName ? returnLinkName : "return to home page";
+
+  const returnHome = () => {
+    router.push("/home?home");
+  };
 
   return (
     <main className={styles.errorPage}>
@@ -27,11 +40,11 @@ export default function ErrorHandler({ errorTitle, errorNotif, returnLink, retur
           <FontAwesomeIcon icon={faQuestionCircle} className={styles.icon} />
           <h2 className={styles.errorTitle}>{title}</h2>
           <p className={styles.errorNotif}>{notif}</p>
-          <Link href={link} className={styles.errorLink}>
+          <div className={styles.errorLink} onClick={returnHome}>
             <h3 className={styles.errorReturn}>{linkName}</h3>
-          </Link>
+          </div>
         </div>
       </div>
     </main>
-  )
+  );
 }
